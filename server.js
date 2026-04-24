62be8834401d0a06efad18066ed173499b9ad2c6
JINSEH <e.p.gamerstv@gmail.com>
2026-04-24 13:33:54 +0800

    Added static public folder to express.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ import { promoCodesRouter } from "./routers/promoCodes.js";
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+
 app.use(express.json());
 //Below code will serve static files once ready.
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/api/auth", authRouter);
 app.use("/api/booking", bookingRouter);
 app.use("/api/rooms", roomsRouter);
